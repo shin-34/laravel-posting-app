f99503b6767e118af057dc27dcd07399f9c52b49
shin-34 <shinmegg@gmail.com>
2024-08-22 22:21:27 +0900

    課題提出

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Auth::user()->posts()->orderBy('created_at', 'asc')->get();
+        $posts = Auth::user()->posts()->orderBy('updated_at', 'desc')->get();
 
         return view('posts.index', compact('posts'));
     }
@@ -62,7 +62,7 @@ class PostController extends Controller
             'title' => 'max:40',
             'content' => 'max:200'
         ]);
-        
+
         if($post->user_id !== Auth::id()) {
             return redirect()->route('posts.index')->with('error_message', '不正なアクセスです。');
         }
